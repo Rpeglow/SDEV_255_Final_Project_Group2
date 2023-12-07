259d4cc68b20d18893c637b0ac5c4ff17533afeb
rpeglow <rpeglow@ivytech.edu>
2023-12-07 14:41:17 -0500

    authentication routes login - logout

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const blogRoutes = require('./routes/blogRoutes');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 //express app
 const app = express();
 
+//connect to mongodb & listen for requests
 const dbURI = 'mongodb+srv://Group2:PoSuSaRe@group2.hedeobb.mongodb.net/Courses';
 mongoose.connect(dbURI)
     .then((result) => {
@@ -19,28 +23,30 @@ mongoose.connect(dbURI)
 //register view engine
 app.set('view engine', 'ejs');
 
-
 //middleware & static files
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(cookieParser());
+
+
+
+
+// routes
+app.get('*', checkUser);
+app.get('/', requireAuth, (req, res) => res.redirect('/blogs'));
+// app.get('/', requireAuth, (req, res) => res.render('home',{ title: 'home' }));
+app.use(authRoutes);
+app.use('/blogs', requireAuth, blogRoutes);
 
 //redirect to /blogs
-app.get('/', (req, res) => {
-    res.redirect('/blogs');
-});
+
 
 //about page
-app.get('/about', (req, res) => {
-    res.render('about',{ title: 'About' });
-});
-
-// blog routes
-app.use('/blogs', blogRoutes);
+app.get('/about', (req, res) => res.render('about',{ title: 'About' }));
 
 //404 page
-app.use((req, res) => {
-    res.status(404).render('404',{ title: '404' });
-});
+app.use((req, res) => res.status(404).render('404',{ title: '404' }));
+
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check json web token exists & is verified
+  if (token) {
+    // PoSuSaRe is the secret key
+    jwt.verify(token, 'PoSuSaRe', (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect('/login');
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    // PoSuSaRe is the secret key
+    jwt.verify(token, 'PoSuSaRe', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+
+module.exports = { requireAuth, checkUser };
